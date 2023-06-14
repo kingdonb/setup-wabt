8efda115bd8de0f581bc407cdb5991981652cafe
Kingdon Barrett <kingdon@weave.works>
2023-06-14 09:39:39 -0400

    deprecated addPath (?)
    how are other extensions handling this now?
    
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const { arch, platform } = require("os")
 const { join } = require("path")
 const { exec: _exec } = require("child_process")
 
+const BASE_URL2 = "https://github.com/WebAssembly/binaryen/releases/download/"
 const BASE_URL = "https://github.com/WebAssembly/wabt/releases/download/"
 const PLATFORM = platform()
 const ARCH = arch()
@@ -12,6 +13,10 @@ const IS_WINDOWS = PLATFORM.startsWith("win")
 
 function wabtdir(version) {
   return join(process.env.HOME || process.env.USERPROFILE, `.wabt_${version}`)
+}
+
+function binaryendir(version) {
+  return join(process.env.HOME || process.env.USERPROFILE, `.binaryen_${version}`)
 }
 
 function exec(cmd) {
@@ -28,10 +33,14 @@ function rm(file) {
   return exec(IS_WINDOWS ? `del /f "${file}"` : `rm -f "${file}"`)
 }
 
+// function addPath(path) {
+//   return exec(...
+// }
+
 function dlurl(version) {
   if (IS_WINDOWS) {
     const os = "windows"
-    return `${BASE_URL}${version}/wabt-${version}-${os}.zip`
+    return `${BASE_URL}${version}/wabt-${version}-${os}.tar.gz`
   } else if (PLATFORM === "darwin") {
     return `${BASE_URL}${version}/wabt-${version}-macos-12.tar.gz`
   } else {
@@ -39,15 +48,33 @@ function dlurl(version) {
   }
 }
 
-async function extract(archive, dir) {
+function dlurl2(version) {
   if (IS_WINDOWS) {
-    await extractZip(archive, dir)
+    const os = "windows"
+    return `${BASE_URL2}version_${version}/binaryen-version_${version}-x86_64-${os}.tar.gz`
+  } else if (PLATFORM === "darwin") {
+    return `${BASE_URL2}version_${version}/binaryen-version_${version}-x86_64-macos.tar.gz`
   } else {
-    await exec(`mkdir -p "${dir}"`)
-    await exec(
-      `tar --extract --gunzip --verbose --strip-components=1 --file="${archive}" --directory="${dir}"`
-    )
+    return `${BASE_URL2}version_${version}/binaryen-version_${version}-x86_64-linux.tar.gz`
   }
+}
+
+// async function extract(archive, dir) {
+//   if (IS_WINDOWS) {
+//     await extractZip(archive, dir)
+//   } else {
+//     await exec(`mkdir -p "${dir}"`)
+//     await exec(
+//       `tar --extract --gunzip --verbose --strip-components=1 --file="${archive}" --directory="${dir}"`
+//     )
+//   }
+// }
+
+async function extract2(archive, dir) {
+  await exec(`mkdir -p "${dir}"`)
+  await exec(
+    `tar --extract --gunzip --verbose --strip-components=1 --file="${archive}" --directory="${dir}"`
+  )
 }
 
 async function main() {
@@ -56,22 +83,30 @@ async function main() {
   try {
     // const version = "1.0.33"
     let version = coerceSemVer(getInput("version"))
+    let version2 = getInput("version2")
 
     if (!version) {
       const release = "v1.0.33" // await latestRelease("WebAssembly", "wabt")
       version = release.replace(/^v/, "")
     }
-    // console.log(version)
+    if (!version2) {
+      version2 = "113"
+    }
 
     const dir = wabtdir(version)
+    const dir2 = binaryendir(version2)
 
     // console.log(dir)
 
     archive = await dltmp(dlurl(version))
+    archive2 = await dltmp(dlurl2(version2))
 
-    await extract(archive, dir)
+    await extract2(archive, dir)
+    await extract2(archive2, dir2)
 
-    addPath(dir)
+    // Add Path does not seem to work, so copy these binaries to your path
+    // addPath(dir)
+    // addPath(dir2)
   } catch (err) {
     setFailed((err && err.message) || "setup_wabt failed")
   } finally {
